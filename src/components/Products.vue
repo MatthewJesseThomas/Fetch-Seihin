d48b7d69546409f99c4f7b7d27b42915acb4af64
MatthewJesseThomas <owlsaintgermain@gmail.com>
2022-12-21 09:07:28 +0200

    'main'

---
+++ src/components/Products.vue
@@ -0,0 +1,55 @@
+<template>
+    <table>
+        <tr>
+          <th>id</th>
+          <th>make</th>
+          <th>spec</th>
+          <th>amount</th>
+          <th>image</th>
+        </tr>
+        <tr v-for="product in products" :key="product.id">
+          <td>{{ product.id }}</td>
+          <td>{{ product.make }}</td>
+          <td>{{ product.spec }}</td>
+          <td>{{ product.amount }}</td>
+          <td>{{ product.image }}</td>
+        </tr>
+      </table>
+</template>
+
+<script>
+import {mapActions, mapGetters } from 'vuex';
+export default {
+    computed: {
+    ...mapGetters(["products"]),    
+    },
+    methods: {
+    ...mapActions(["fetchAllProducts"]),
+    },
+    created() {
+        this.fetchAllProducts();
+    },
+};
+</script>
+
+<style scoped>
+table {
+    font-family: arial, sans-serif;
+    border-collapse: collapse;
+    width: 100%;
+    max-width: 30rem;
+    margin: auto;
+    align-content: center;
+    justify-content: center;
+}
+
+td,th {
+    border: 1px solid #333;
+    text-align: left;
+    padding: 8px;
+}
+
+tr:nth-child(even) {
+    background-color: #dddddd ; 
+}
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,23 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+const routes = [
+  
+  {
+    path: "/products",
+    name: "products",
+    component: () => import("@/views/ProductsView.vue"),
+    
+  },
+  {
+    path: "/admin",
+    name: "admin",
+    component: () => import("../views/AdminView.vue"),
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes,
+});
+
+export default router;
